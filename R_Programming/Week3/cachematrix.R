34ba822791eb8baf437bcb29acb7f0acc7ca957c
ancazugo <33363093+ancazugo@users.noreply.github.com>
2020-04-28 22:18:50 -0500

    Added the Folder to Week3 and its assignment

---
+++ R_Programming/Week3/cachematrix.R
@@ -0,0 +1,31 @@
+## Create a matrix that gets and sets its value and the inverse of it
+
+makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+}
+
+
+## Runs solve() (inverse of matrix) on the matrix cache'd by makeCachematrix()
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
+}
